419948aa23b78c2c4c926e63047f689089e7058e
subbuplatina <45375506+subbuplatina@users.noreply.github.com>
2022-07-20 08:14:55 +0530

    Update ActionsDemo.java

--- Day27/ActionsDemo.java
+++ Day27/ActionsDemo.java
@@ -1,1 +1,16 @@
-Test file
+public class ActionsDemo {
+
+	public static void main(String[] args) throws InterruptedException {
+		System.setProperty("webdriver.chrome.driver", ".//Resources//chromedriver.exe");
+		WebDriver driver  = new ChromeDriver();
+		driver.manage().window().maximize();
+		Actions act = new Actions(driver);
+		driver.get("https://www.online.citibank.co.in/");
+		Thread.sleep(2000);
+		WebElement lnkCreditCards = driver.findElement(By.xpath("//a[@title='Credit Cards' and @id='topMnucreditcards']"));
+		act.moveToElement(lnkCreditCards).perform();
+		Thread.sleep(10000);
+		driver.close();
+		driver.quit();
+	}
+}
