dd7f9d95fa0a076b7458da363e7ea4030f5f5459
subbuplatina <45375506+subbuplatina@users.noreply.github.com>
2022-08-10 18:14:50 +0530

    Create Selenium1.py

---
+++ Day43/Selenium1.py
@@ -0,0 +1,16 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.keys import Keys
+
+driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+driver.get("https://www.google.co.in")
+search = driver.find_element(By.XPATH, '//input[@name="q"]')
+search.clear()
+search.send_keys("New James Bond Movies")
+search.send_keys(Keys.ENTER)
+
+print(driver.current_url)
+driver.close()
+driver.quit()
